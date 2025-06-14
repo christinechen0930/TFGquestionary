749fa962d6cabba50c2fd6b8f8f057c5b31961a6
christinechen0930 <d11230619@gapps.fg.tp.edu.tw>
2025-06-14 09:44:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import os import re import requests import torch import streamlit as st from bs4 import BeautifulSoup from urllib.parse import urljoin, unquote, urlparse from sentence_transformers import SentenceTransformer, util import fitz  # PyMuPDF
+import os
+import re
+import requests
+import torch
+import streamlit as st
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin, unquote, urlparse
+from sentence_transformers import SentenceTransformer, util import fitz  # PyMuPDF
 
 ====== 設定 API Key ======
 
